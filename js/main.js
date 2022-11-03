4357b00033b886e39db9fe5cf7d61c192db2f6df
Pablo Ortiz <pablortiz78@MacBook-Pro-de-Pablo.local>
2022-11-02 23:45:46 -0400

    more responsive

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(() => {
+
+const toggleMenuElement = document.getElementById('toggle_menu');
+const mainMenuElement = document.getElementById('main_menu');
+
+toggleMenuElement.addEventListener('click',() => {
+    mainMenuElement.classList.toggle('main_menu--show');
+});
+
+})()
